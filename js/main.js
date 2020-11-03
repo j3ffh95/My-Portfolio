59e583d10ca07de21641f91cd115d728b4579cfd
j3ffh95 <j3ffh95@gmail.com>
2020-11-02 23:37:10 -0500

    FIxed the bug in the navbar hamburger

--- js/main.js
+++ js/main.js
@@ -1,38 +1,39 @@
 // /* This functions make the sidebar appear when is in mobile platform=============================== */
 
-function openSlideMenu(){
-  document.getElementById('side-menu').style.width = '200px';
-  document.getElementById('main').style.marginLeft = '200px';
+function openSlideMenu() {
+  document.getElementById("side-menu").style.width = "200px";
+  document.querySelector(".navbar a").style.display = "none";
+  document.getElementById("main").style.marginLeft = "200px";
 }
 
-function closeSlideMenu(){
-  document.getElementById('side-menu').style.width = '0';
-  document.getElementById('main').style.marginLeft = '0';
+function closeSlideMenu() {
+  document.querySelector(".navbar a").style.display = "block";
+  document.getElementById("side-menu").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
 
 // /* end of function =============================== */
 
-
 // /* JQUERY FUNCTION FOR GALLERY =============================== */
 
-$(document).ready(function(){
-  $('.next').on('click', function(){
-    var currentImg = $('.active');
+$(document).ready(function () {
+  $(".next").on("click", function () {
+    var currentImg = $(".active");
     var nextImg = currentImg.next();
 
-    if(nextImg.length){
-      currentImg.removeClass('active').css('z-index', -10);
-      nextImg.addClass('active').css('z-index', 10);
+    if (nextImg.length) {
+      currentImg.removeClass("active").css("z-index", -10);
+      nextImg.addClass("active").css("z-index", 10);
     }
   });
 
-  $('.prev').on('click', function(){
-    var currentImg = $('.active');
+  $(".prev").on("click", function () {
+    var currentImg = $(".active");
     var prevImg = currentImg.prev();
 
-    if(prevImg.length){
-      currentImg.removeClass('active').css('z-index', -10);
-      prevImg.addClass('active').css('z-index', 10);
+    if (prevImg.length) {
+      currentImg.removeClass("active").css("z-index", -10);
+      prevImg.addClass("active").css("z-index", 10);
     }
   });
 });
