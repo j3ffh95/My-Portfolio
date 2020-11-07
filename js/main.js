ffc0b65d2f38905e972500b051b65ccb7c6838db
j3ffh95 <j3ffh95@gmail.com>
2020-11-06 23:18:24 -0500

    remove jquery section

--- js/main.js
+++ js/main.js
@@ -13,28 +13,3 @@ function closeSlideMenu() {
 }
 
 // /* end of function =============================== */
-
-// /* JQUERY FUNCTION FOR GALLERY =============================== */
-
-$(document).ready(function () {
-  $(".next").on("click", function () {
-    var currentImg = $(".active");
-    var nextImg = currentImg.next();
-
-    if (nextImg.length) {
-      currentImg.removeClass("active").css("z-index", -10);
-      nextImg.addClass("active").css("z-index", 10);
-    }
-  });
-
-  $(".prev").on("click", function () {
-    var currentImg = $(".active");
-    var prevImg = currentImg.prev();
-
-    if (prevImg.length) {
-      currentImg.removeClass("active").css("z-index", -10);
-      prevImg.addClass("active").css("z-index", 10);
-    }
-  });
-});
-// /* END OF JQUERY FUNCTION FOR GALLERY =============================== */
